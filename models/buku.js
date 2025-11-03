f5b206112679f04ac0cb24248a045f8d73648761
“DhandhaDendriyaE” <dhanda.dendriya.ft22@mail.umy.ac.id>
2025-11-03 08:53:31 +0700

    men set up sequelize

---
+++ models/buku.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+    const Buku = sequelize.define('Buku', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,   
+            autoIncrement: true
+        },
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        pengarang: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        tahun_terbit: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },           
+        bidang
+    });
+    return Buku;
+}
