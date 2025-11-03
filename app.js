13943fcd9f4d653e0fe54f36527cf9b6632576a7
“DhandhaDendriyaE” <dhanda.dendriya.ft22@mail.umy.ac.id>
2025-11-03 15:11:48 +0700

    typo

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.put('/buku/:id', async (req, res) => {
 app.delete('/buku/:id', async (req, res) => {
     const id = req.params.id;
     try {
-        const buku = await db.buku.findByPk(id);
+        const buku = await db.Buku.findByPk(id);
         if (!buku) {
             return res.status(404).send({ message: 'Buku not found' });
         }
